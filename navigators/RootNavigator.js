c63f4cb6f6cd43b421f6efc4e5639699dae67f88
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-08-12 16:25:27 +0300

    verifying a user is authenticated before or not and allow him to the home page

--- navigators/RootNavigator.js
+++ navigators/RootNavigator.js
@@ -4,36 +4,34 @@ import { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useDispatch } from "react-redux";
 import { login } from "../state/auth/authSlice";
-import Home from "../screens/home/Home";
 
 const RootNavigator = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   getUser()
-  //     .then((res) => {
-  //       const { user, token } = JSON.parse(res);
-  //       if (res) {
-  //         dispatch(login({ user, token }));
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getUser()
+      .then((res) => {
+        const { user, token } = JSON.parse(res);
+        if (res) {
+          dispatch(login({ user, token }));
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  // const getUser = async () => {
-  //   try {
-  //     const token = await AsyncStorage.getItem("token");
-  //     return token;
-  //   } catch (error) {
-  //     console.error("Error retrieving token:", error);
-  //     return null;
-  //   }
-  // };
+  const getUser = async () => {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      return token;
+    } catch (error) {
+      console.error("Error retrieving token:", error);
+      return null;
+    }
+  };
   return (
     <NavigationContainer>
-      {/* <Home /> */}
       <DrawerNavigator />
     </NavigationContainer>
   );
