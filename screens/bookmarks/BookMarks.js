cccfdce8652d8724f054754f1f49e0290593d586
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-11-11 15:02:57 +0300

    adding bookmark page

--- screens/bookmarks/BookMarks.js
+++ screens/bookmarks/BookMarks.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet, Alert, Image } from "react-native";
 
 const BookMarks = () => {
+  const handleButtonClick = () => {
+    Alert.alert("Feature Coming Soon!", "Stay tuned for updates.");
+  };
+
   return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <Text style={{ marginVertical: "auto", marginHorizontal: "auto" }}>
-        It's under development, comming soon
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Bookmarks Feature is Under Development</Text>
+      <Button title="Notify Me" onPress={handleButtonClick} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  text: {
+    marginVertical: 20,
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
 
 export default BookMarks;
